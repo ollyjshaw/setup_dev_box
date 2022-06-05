2f21b7ed60810f33c9cb1980661922616d71e210
Oliver Shaw <ollyjshaw@gmail.com>
2022-06-05 15:59:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #Do this first!
 #curl -O -J  https://raw.githubusercontent.com/ollyjshaw/setup_dev_box/main/install.sh
 
+set -e
+
 #update first
-echo "updating first
+echo "updating first"
 apt-get -y update
 apt-get -y upgrade
 
